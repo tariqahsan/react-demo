0739b652a88f7f907b9e55d9a4ebfbae93b6a162
Tariq Ahsan <tariqahsan@yahoo.com>
2019-07-03 12:13:11 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Form, Input, Button, Checkbox } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Let's get to it ...
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Login from './components/login/NormalLoginForm';
+import ButtonTest from './components/test/ButtonTest';
+import PropTest from './components/test/PropTest';
+import StatePropTest2 from './components/test/StatePropTest2';
+import TodoApp from './components/todo/TodoApp';
+import TodoApp1 from './components/todo/TodoApp1';
+import TodoAntD2 from './components/todo/TodoAntD2';
+import TodoAntD3 from './components/todo/TodoAntD3';
+import RegistrationForm from './components/todo/RegistrationForm';
+import CollectionsPage from './components/todo/CollectionsPage';
+import DynamicFieldSet from './components/todo/DynamicFieldSet';
+import FormCustomizedValidation from './components/todo/FormCustomizedValidation';
+import Complete from './components/todo/AutoComplete';
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
+class App extends Component {
+  render() {
+    return (
+      <FormCustomizedValidation/>    
+    );
+  }
 }
 
 export default App;
